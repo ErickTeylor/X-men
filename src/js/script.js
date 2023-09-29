d6b9e7eb2e2f66b9f39bfa862a2ea6de78466e57
Erick Teylor <erickteylor.github@gmail.com>
2023-09-29 08:53:29 -0300

    Primeiro Commit

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+let character = document.getElementsByClassName("character");
+let name = document.getElementsByTagName("character-name");
+let description = document.getElementsByClassName("character-text");
+
